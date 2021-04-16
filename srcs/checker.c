675358c06f50ccd944c20391a956178e17c5c6b9
Jaqueline <jaguilarodrigues>
2021-04-15 23:40:08 -0300

    fix norm

--- srcs/checker.c
+++ srcs/checker.c
@@ -6,7 +6,7 @@
 /*   By: jaqrodri <jaqrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 18:33:24 by jaqrodri          #+#    #+#             */
-/*   Updated: 2021/04/15 23:03:21 by jaqrodri         ###   ########.fr       */
+/*   Updated: 2021/04/15 23:39:37 by jaqrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,6 @@ void	start_stack(t_stack *s)
 int	main(int argc, char *argv[])
 {
 	t_stack	stack_a;
-	// t_stack	stack_b;
 
 	if (argc <= 1)
 		return (error());
@@ -81,15 +80,9 @@ int	main(int argc, char *argv[])
 		return (error());
 	print_stack(&stack_a);
 	ft_putstr_fd("\n", 1);
-	
 	rotate(&stack_a);
-	// start_stack(&stack_b);
-	// push_stack(9, &stack_b);
-	// pa(&stack_a, &stack_b);
-	
 	print_stack(&stack_a);
 	ft_putstr_fd("\n", 1);
-	// print_stack(&stack_b);
 	free_stack(&stack_a);
 	return (0);
 }
